2d2031c85945bea19254f9e2bde7e4c24f758c26
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2025-06-05 21:54:13 +0100

    first commit

---
+++ client.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void tratar_sinal(int sig)
+{
+    if (sig == 30)
+        printf("Recebi o signal %d\n ", sig);
+    else if (sig == 31)
+        printf("Hello Wold %d\n", sig);
+
+}
+
+int main()
+{
+    printf("Server running (PID:%d)\n", getpid());
+
+    signal(SIGUSR1, tratar_sinal);
+    signal(SIGUSR2, tratar_sinal);
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
