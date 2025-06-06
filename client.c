89e461d11919a065e75ca838de7654a6838342c3
bbras81 <miguelazevedodg@gmail.com>
2025-06-06 09:22:07 +0100

    Auto-commit: 2025-06-06 09:22:07

--- client.c
+++ client.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
 #include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
 
-void tratar_sinal(int sig)
+void	tratar_sinal(int sig)
 {
-    if (sig == 30)
-        printf("Recebi o signal %d\n ", sig);
-    else if (sig == 31)
-        printf("Hello Wold %d\n", sig);
-
+	if (sig == 30)
+		printf("Recebi o signal %d\n ", sig);
+	else if (sig == 31)
+		printf("Hello Wold %d\n", sig);
 }
 
-int main()
+int	main(void)
 {
-    printf("Server running (PID:%d)\n", getpid());
-
-    signal(SIGUSR1, tratar_sinal);
-    signal(SIGUSR2, tratar_sinal);
-    while(1)
-    {
-        sleep(1);
-    }
-    return 0;
+	printf("Server running (PID:%d)\n", getpid());
+	signal(SIGUSR1, tratar_sinal);
+	signal(SIGUSR2, tratar_sinal);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
