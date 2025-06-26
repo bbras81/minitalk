524e49b9c863b48abd315b445ad740cb1667fa0f
bbras81 <miguelazevedodg@gmail.com>
2025-06-26 11:19:53 +0100

    Auto-commit: 2025-06-26 11:19:53

--- client.c
+++ client.c
@@ -16,6 +16,7 @@ void	sending_signals(int pid, char message)
 {
 	int	i;
 
+	i = -1;
 	while (++i < 8)
 	{
 		if (((unsigned char)(message >> (7 - i)) & 1) == 0)
@@ -24,13 +25,12 @@ void	sending_signals(int pid, char message)
 			kill(pid, SIGUSR2);
 		usleep(200);
 	}
-}
-i = 0;
-while (i++ < 8)
-{
-	kill(pid, SIGUSR1);
-	usleep(200);
-}
+	i = 0;
+	while (i++ < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(200);
+	}
 }
 
 int	main(int argc, char **argv)
@@ -41,14 +41,14 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 	{
 		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-		return (1);
+		return (EXIT_FAILURE);
 	}
 	server_pid = ft_atoi(argv[1]);
 	if (server_pid <= 0)
-		return (1);
+		return (EXIT_FAILURE);
 	message = argv[2];
 	if (message[0] == '\0')
-		return (1);
+		return (EXIT_FAILURE);
 	while (*message)
 		sending_signals(server_pid, *message++);
 	ft_printf("Message sent to PID %d\n", server_pid);
