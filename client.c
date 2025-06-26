5550dc8cd84c5b800474deadbd9bea53b116d0b1
Zedro <zedro@zedro.com>
2025-06-26 13:07:15 +0100

    minitalk

--- client.c
+++ client.c
@@ -3,70 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
+/*   By: brunmigu <brunmigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/06 13:08:26 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/06/06 13:14:46 by brunmigu         ###   ########.fr       */
+/*   Created: 2025/06/26 12:49:06 by brunmigu          #+#    #+#             */
+/*   Updated: 2025/06/26 12:52:18 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
-#include <unistd.h>
-
-volatile sig_atomic_t	g_client = BUSY;
-void	c_handler(int sig)
-{
-	if (sig == SIGUSR1)
-	{
-		g_client = READY;
-	}
-}
-
-void	sending_signals(int pid, char message)
-{
-	int	i;
-
-	i = -1;
-	while (++i < 8)
-	{
-		if (((unsigned char)(message >> (7 - i)) & 1) == 0)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		while (g_client == BUSY)
-			usleep(42);
-	}
-	i = 0;
-	while (i++ < 8)
-	{
-		kill(pid, SIGUSR1);
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	int					server_pid;
-	char				*message;
-	struct sigaction	sa;
-
-	sa.sa_handler = c_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	if (argc != 3)
-	{
-		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-		return (EXIT_FAILURE);
-	}
-	server_pid = ft_atoi(argv[1]);
-	if (server_pid <= 0)
-		return (EXIT_FAILURE);
-	message = argv[2];
-	if (message[0] == '\0')
-		return (EXIT_FAILURE);
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	while (*message)
-		sending_signals(server_pid, *message++);
-	ft_printf("Message sent to PID %d\n", server_pid);
-	return (EXIT_SUCCESS);
-}
--- server.c
+++ server.c
@@ -3,49 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
+/*   By: brunmigu <brunmigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/26 10:15:59 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/06/26 10:22:09 by brunmigu         ###   ########.fr       */
+/*   Created: 2025/06/26 12:49:36 by brunmigu          #+#    #+#             */
+/*   Updated: 2025/06/26 12:53:41 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	sig_handler(int signum, siginfo_t *info, void *ucontext)
+int main()
 {
-	static unsigned char	current_char = 0;
-	static int				bit_index = 0;
-	static pid_t			client_pid;
-
-	client_pid = info->si_pid;
-	(void)ucontext;
-	current_char <<= 1;
-	if (signum == SIGUSR2)
-		current_char |= 1;
-	bit_index++;
-	if (bit_index == 8)
-	{
-		write(1, &current_char, 1);
-		current_char = 0;
-		bit_index = 0;
-	}
-	kill(client_pid, SIGUSR1);
-}
-
-int	main(void)
-{
-	struct sigaction	sa;
-
-	sa.sa_sigaction = sig_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	printf("Server PID: %d\n", getpid());
 	while (1)
 	{
 		pause();
 	}
-	return (EXIT_SUCCESS);
+
+
 }
