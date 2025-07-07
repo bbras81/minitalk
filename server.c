fd114481155985705eaadf95f43d99de6bce22b6
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2025-07-07 20:51:56 +0100

    Minitalk

--- server.c
+++ server.c
@@ -3,37 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunmigu <brunmigu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/04 11:56:25 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/07/05 12:45:17 by brunmigu         ###   ########.fr       */
+/*   Created: 2025/07/06 16:08:28 by brunmigu          #+#    #+#             */
+/*   Updated: 2025/07/06 16:16:30 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
 #include "minitalk.h"
 
-static void	handler(int signal, siginfo_t *info, void *more_info)
+static void	get_int(int signal, int len, int bit)
 {
-	static int	len = 0;
-	static int	bit = 0;
-	static char	*message;
-
-	(void)info;
-	(void)more_info;
 	len <<= 1;
 	if (signal == SIGUSR1)
 		len |= 1;
 	bit++;
+}
+static char	get_char(int signal, int len, int bit)
+{
+	len <<= 1;
+	if (signal == SIGUSR1)
+		len |= 1;
+	bit++;
+	return (bit);
+}
+
+static void	handler(int signal, siginfo_t *info, void *more_info)
+{
+	static int	len;
+	static int	bit;
+
+	(void)info;
+	(void)more_info;
+	get_int(signal, len, bit);
 	if (bit == 32)
 	{
 		ft_printf("%d", len);
 		bit = 0;
 		len = 0;
-	}
-	if (bit == 8)
-	{
-    
 	}
 }
 
